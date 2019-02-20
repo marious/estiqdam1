5c096c977a1849f8f8f3c38b7b9ab7a38c8be87b
marious <mohamedraya410@gmail.com>
2019-02-20 11:24:35 +0300

    second commit

--- mytwitter/test.php
+++ mytwitter/test.php
@@ -1,10 +1,7 @@
 <?php
 include __DIR__ . '/includes/setup.php';
 
-$consumer_key = 'at0Hd570r40d3bJieHGX2nUXj';
-$consumer_secret = 'RWJK46WlLURyq2V4TwIxbMyYhWrR8DshmJomCt2xUCg74jh8Qf';
-$access_token = '914409145842970624-wIrM5sO1lVhudny1Rardc8Cd7S6WYUH';
-$access_token_secret = 'JPGJiQ6uezM0Erpp6pYvgMTljYsQCC0aOAcOef6SilvnI';
+
 
 $twitter = new \Abraham\TwitterOAuth\TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
 
