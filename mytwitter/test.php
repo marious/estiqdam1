facd9c17904246907cc95d91a0308940fd785593
marious <mohamedraya410@gmail.com>
2019-03-03 19:51:49 +0300

    second commit

--- mytwitter/test.php
+++ mytwitter/test.php
@@ -1,3 +1,4 @@
 <?php
-$data = new mysqli("mysql.hostinger.co.uk","u463543975_mash","karakesek","u463543975_dubs");
-var_dump($data);
+
+$con = new mysqli('mysql.hostinger.co.uk','u125539847_expre','mghkl1234', 'u125539847_expre');
+var_dump($con);
