992150fd151e63e4d7aa89dab6e76661778f432a
marious <mohamedraya410@gmail.com>
2019-03-03 19:49:45 +0300

    second commit

--- mytwitter/test.php
+++ mytwitter/test.php
@@ -1,15 +1,3 @@
 <?php
-include __DIR__ . '/includes/setup.php';
-
-
-
-$twitter = new \Abraham\TwitterOAuth\TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
-
-$trends = $twitter->get('trends/place', array('id' => 23424938,));
-$db = DB::connect();
-foreach ($trends[0]->trends as $trend) {
-    $query = "INSERT INTO hashtags (hashtag) VALUES(:hashtag)";
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(':hashtag', $trend->name);
-    $stmt->execute();
-}
+$data = new mysqli("mysql.hostinger.co.uk","u463543975_mash","karakesek","u463543975_dubs");
+var_dump($data);
