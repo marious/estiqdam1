58b38334515bcdcb50019252f50d6fed0dc46515
marious <mohamedraya410@gmail.com>
2019-04-22 17:48:43 +0300

    second commit

--- mytwitter/admin/index.php
+++ mytwitter/admin/index.php
@@ -28,29 +28,33 @@ $users = $userModel->getAll();
 
     <table class="table table-bordered table-striped">
         <thead>
-            <tr>
-                <th width="20%">Screen Name</th>
-                <th width="15%">Profile Image</th>
-                <th width="2%">Following</th>
-                <th width="2%">Followers</th>
-                <th>Proxy</th>
-                <th>Action</th>
-            </tr>
+        <tr>
+            <th width="4%">#</th>
+            <th width="12%">Screen Name</th>
+            <th width="2%">Following</th>
+            <th width="2%">Followers</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Phone</th>
+            <th>Proxy</th>
+            <th>Action</th>
+        </tr>
         </thead>
         <?php if ($users && count($users)): ?>
-        <tbody>
-            <?php foreach ($users as $user): ?>
+            <tbody>
+            <?php $i = 1; foreach ($users as $user): ?>
                 <tr>
+                    <td><?= $i; ?></td>
                     <td><?= $user['screen_name'] ?></td>
-                    <td>
-                        <img src="<?= $user['profile_image_url'] ?>" alt="<?= $user['screen_name'] ?>" class="img-responsive">
-                    </td>
                     <td>
                         <?= $user['friends_count'] ?>
                     </td>
                     <td>
                         <?= $user['followers_count'] ?>
                     </td>
+                    <td><?= $user['email'] ?></td>
+                    <td><?= $user['password'] ?></td>
+                    <td><?= $user['phone'] ?></td>
                     <td>
                         <?= $user['proxy'] ?>
                     </td>
@@ -59,35 +63,49 @@ $users = $userModel->getAll();
 
                     </td>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
+                <?php $i++; endforeach; ?>
+            </tbody>
         <?php endif; ?>
     </table>
 </div>
 
 
-    <div id="userModal" class="modal fade">
-        <div class="modal-dialog">
-            <form method="post" id="user_form" enctype="multipart/form-data">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">Update User</h4>
-                    </div>
-                    <div class="modal-body">
-                        <label>Enter Proxy Server</label>
+<div id="userModal" class="modal fade">
+    <div class="modal-dialog">
+        <form method="post" id="user_form" enctype="multipart/form-data">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Update User</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Proxy Server</label>
                         <input type="text" name="proxy" id="proxy_user" class="form-control">
                     </div>
-                    <div class="modal-footer">
-                        <input type="hidden" name="user_id" id="user_id" />
-                        <input type="hidden" name="operation" id="operation" />
-                        <input type="submit" name="action" id="action" class="btn btn-success" value="Add" />
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <div class="form-group">
+                        <label for="">Email</label>
+                        <input type="email" name="email" class="form-control" id="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Password</label>
+                        <input type="text" name="password" class="form-control" id="password">
+                    </div>
+                    <div class="form-group">
+                        <label for="phone">Phone</label>
+                        <input type="text" name="phone" class="form-control" id="phone">
                     </div>
                 </div>
-            </form>
-        </div>
+                <div class="modal-footer">
+                    <input type="hidden" name="user_id" id="user_id" />
+                    <input type="hidden" name="operation" id="operation" />
+                    <input type="submit" name="action" id="action" class="btn btn-success" value="Add" />
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </form>
     </div>
+</div>
 
 <?php
 $edit_script = true;
@@ -100,18 +118,18 @@ $edit_script = true;
         var proxy = $('#proxy_user').val();
         var user_id = $('#user_id').val();
         // if (proxy != '') {
-            $.ajax({
-                url: "<?php echo URL_ROOT . 'admin/ajax/update_proxy.php' ?>",
-                method: "POST",
-                data: new FormData(this),
-                contentType: false,
-                processData: false,
-                success: function(data) {
-                    $('#user_form')[0].reset();
-                    $('#userModal').hide();
-                    location.reload(true);
-                }
-            });
+        $.ajax({
+            url: "<?php echo URL_ROOT . 'admin/ajax/update_proxy.php' ?>",
+            method: "POST",
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+            success: function(data) {
+                $('#user_form')[0].reset();
+                $('#userModal').hide();
+                location.reload(true);
+            }
+        });
         // }
     });
 
@@ -127,6 +145,9 @@ $edit_script = true;
                 $('#userModal').modal('show');
                 $('#user_id').val(user_id);
                 $('#proxy_user').val(data.proxy);
+                $('#phone').val(data.phone);
+                $('#email').val(data.email);
+                $('#password').val(data.password);
             }
         });
     });
