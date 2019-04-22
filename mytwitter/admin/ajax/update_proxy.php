72d56e291ebe74cca229b7b8e0f915e98295fd55
marious <mohamedraya410@gmail.com>
2019-04-22 17:48:18 +0300

    second commit

--- mytwitter/admin/ajax/update_proxy.php
+++ mytwitter/admin/ajax/update_proxy.php
@@ -1,12 +1,18 @@
 <?php
 include __DIR__ . '/../../includes/setup.php';
 $user_id = $_POST['user_id'];
-$proxy = $_POST['proxy'];
+$proxy = trim($_POST['proxy']);
+$email = trim($_POST['email']);
+$password = trim($_POST['password']);
+$phone = trim($_POST['phone']);
 
 $db = DB::connect();
 
-$query = "UPDATE users set proxy = :proxy where id = :id";
+$query = "UPDATE users set proxy = :proxy, phone=:phone, email=:email, `password`=:password where id = :id";
 $stmt = $db->prepare($query);
 $stmt->bindValue(':proxy', $proxy);
+$stmt->bindValue(':phone', $phone);
+$stmt->bindValue(':email', $email);
+$stmt->bindValue(':password', $password);
 $stmt->bindValue(':id', $user_id);
 return $stmt->execute();
