27d0e7a3f7345dd81c5641f8c540673e3ac97d6c
marious <mohamedraya410@gmail.com>
2019-04-23 10:01:31 +0300

    second commit

---
+++ mytwitter/refresh.php
@@ -0,0 +1,22 @@
+<?php
+use Abraham\TwitterOAuth\TwitterOAuth;
+require 'includes/setup.php';
+$db = DB::connect();
+
+$stmt = $db->prepare('SELECT * FROM users');
+$stmt->execute();
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+$settings = new \MyApp\Models\Setting();
+$twitterKeys = $settings->get('my_twitter_app');
+
+$connection = new TwitterOAuth($twitterKeys['consumer_key'], $twitterKeys['consumer_secret']);
+
+foreach ($users as $user) {
+    $user_info = $connection->get('users/show', ['user_id' => $user['id']]);
+    $stmt = $db->prepare('UPDATE users SET name = :name WHERE id = :id');
+    $stmt->bindValue(':name', $user_info->name);
+    $stmt->bindValue(':id', $user['id']);
+    $stmt->execute();
+}
