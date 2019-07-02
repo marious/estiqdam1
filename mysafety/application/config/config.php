cbfde68f57f6e85b883fbfd9d13251b44ebfa140
marious <mohamedraya410@gmail.com>
2019-07-02 22:07:12 +0300

    second commit

--- mysafety/application/config/config.php
+++ mysafety/application/config/config.php
@@ -35,7 +35,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/mysafety';
+$config['base_url'] = 'http://estgdam1.com/mysafety';
 
 /*
 |--------------------------------------------------------------------------
