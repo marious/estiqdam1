39fb3c8fa573b5deb4dc27842671465ca5c85243
marious <mohamedraya410@gmail.com>
2019-10-22 10:05:12 +0300

    second commit

--- fanoos/application/language/english/clients_lang.php
+++
@@ -1,3 +0,0 @@
-<?php
-$lang['edit_client'] = 'Edit Client';
-$lang['add_new_client'] = 'Add New Client';
--- fanoos/application/language/english/msg_lang.php
+++
@@ -1,5 +0,0 @@
-<?php
-$lang['sorry_you_cannot_delete_used_by_other'] = 'Sorry! you can not delete this record, It already used';
-$lang['delete_success'] = 'Your Record Has Been Deleted Successfully';
-$lang['there_is_no_record_for_display'] = 'There is no record for display';
-$lang['qty_is_greater_than_inventory'] = 'The Qty is greater than products in inventory';
